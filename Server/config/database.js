385f407bd25f9436d7a856646e6153c6c598fe7b
pawan <pawankumaryadavvns123@gmail.com>
2025-09-06 01:03:13 +0530

    ok

--- Server/config/database.js
+++ Server/config/database.js
@@ -2,10 +2,11 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 exports.connect =()=>{
-    mongoose.connect(process.env.MONGODB_URL,{
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
+    mongoose.connect(process.env.MONGODB_URL)
+        // ,{
+        // useNewUrlParser: true,
+        // useUnifiedTopology: true,
+    // })
     .then(()=> console.log("DB Connected Successfully"))
     .catch((error)=>{
         console.log("DB Connection Failed");
